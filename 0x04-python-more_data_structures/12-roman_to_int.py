aae0e7aedee3fc2873eb84c3167506ca62d3aad9
kupamboho <mboho.kupa@gmail.com>
2022-12-07 23:09:53 +0000

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-
 def to_subtract(list_num):
-
     to_sub = 0
     max_list = max(list_num)
 
@@ -11,14 +9,12 @@ def to_subtract(list_num):
 
     return (max_list - to_sub)
 
+
 def roman_to_int(roman_string):
-
     if not roman_string:
-
         return 0
 
     if not isinstance(roman_string, str):
-
         return 0
 
     rom_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
